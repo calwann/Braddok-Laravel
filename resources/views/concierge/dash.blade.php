9a163f0e65bfd007beb358331f103207c4859c22
Calwann de Souza Freire <calwann@gmail.com>
2021-01-17 15:06:05 -0300

    17/01/2021 - v0.27

--- resources/views/concierge/dash.blade.php
+++ resources/views/concierge/dash.blade.php
@@ -14,34 +14,6 @@
                 </div>
                 <div class="card-content hoverable">
                     <div class="row">
-                        <div class="input-field col m4 s12">
-                            <select id="registerType" name="registerType">
-                                <option value="" disabled="disabled" {{ old('registerType') == '' ? 'selected' : '' }}>
-                                </option>
-                                <option value="1" {{ old('registerType') == '1' ? 'selected' : '' }}>Entrada</option>
-                                <option value="2" {{ old('registerType') == '2' ? 'selected' : '' }}>Saída</option>
-                            </select>
-                            <label for="registerType">Lançamento</label>
-                        </div>
-                        <div class="input-field col m8 s12">
-                            <select multiple id="usersId" name="usersId[]">
-                            </select>
-                            <label for="usersId">Militares</label>
-                        </div>
-                        <div class="input-field col m6 s12">
-                            <i class="datepicker material-icons prefix pointer tooltipped" data-position="top"
-                                data-tooltip="Escolher Data">date_range</i>
-                            <input id="date" type="text" class="datepicker-control validate date-validation" name="date"
-                                value="{{ old('date') }}" placeholder="00/00/0000">
-                            <label for="date">Data</label>
-                        </div>
-                        <div class="input-field col m6 s12">
-                            <i class="timepicker material-icons prefix pointer tooltipped" data-position="top"
-                                data-tooltip="Escolher Hora">access_time</i>
-                            <input id="time" type="text" class="timepicker-control validate time-validation" name="time"
-                                value="{{ old('time') }}" placeholder="00:00">
-                            <label for="time">Hora</label>
-                        </div>
                     </div>
                     <div class="row">
                         <button class="waves-effect waves-light btn teal darken-1 left-align hoverable" type="submit">Enviar</button>
